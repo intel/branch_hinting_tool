b635f0bda880e5f5810a01f27624eefb3290d340
csamoila <gabriel.c.samoila@intel.com>
2015-09-07 18:00:16 +0300

    cleanup

--- branch_tagger/apply_fsm_file.py
+++
@@ -1,9 +0,0 @@
-from states_impl import OutContext
-from fsm import FSM
-
-
-# Applies the finite state machine, starting from state OutContext
-
-class ApplyFSMFile(FSM):
-    def __init__(self):
-        FSM.__init__(self, OutContext())
